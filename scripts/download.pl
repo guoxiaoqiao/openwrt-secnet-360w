409a810364af51c6e709c4cbdb271c2bbc59d0d7
Joe Guo <qiao@huayang-box.huayua.icu>
2021-02-08 00:21:29 +0800

    改用阿里云服务器下载内核

--- scripts/download.pl
+++ scripts/download.pl
@@ -236,6 +236,7 @@ foreach my $mirror (@ARGV) {
 			push @extra, "$extra[0]/longterm/v$1";
 		}
 		foreach my $dir (@extra) {
+			push @mirrors, "https://mirrors.aliyun.com/linux-kernel/v5.x/";
 			push @mirrors, "https://cdn.kernel.org/pub/$dir";
 			push @mirrors, "https://download.xs4all.nl/ftp.kernel.org/pub/$dir";
 			push @mirrors, "https://mirrors.mit.edu/kernel/$dir";
